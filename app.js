d505ebb3dc1a2f77e2a3a8d4a3843d09ffc304cc
tenerifewonders <tenerifewonders@gmail.com>
2026-05-09 15:56:48 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Ruta base de tus audios en Supabase
 const AUDIO_BASE = "https://xzymbvnljudyypdyuisf.supabase.co/storage/v1/object/public/audioguides/";
 
-let puntos = [];
+let audios = [];
 let indice = 0;
 
-// Cargar puntos + audios
-async function cargarPuntosAudio() {
+// Cargar audios según número de puntos del GeoJSON
+async function cargarAudios() {
   const res = await fetch('./ANAGA.geojson');
   const data = await res.json();
 
@@ -16,18 +16,14 @@ async function cargarPuntosAudio() {
 }
 
 async function loadGuide() {
-  puntos = await cargarPuntosAudio();  // ← AHORA SE CARGA ANTES DE USARLO
+  audios = await cargarAudios();
 
   document.getElementById("guide-title").textContent = "Anaga";
   document.getElementById("guide-description").textContent = "The Pirate Coast";
-}
 
-// Reproducir audio correcto
-function seleccionarCapitulo(i) {
-  indice = i;
+  // Reproducir automáticamente el primer audio
   const player = document.getElementById("player");
-  player.src = puntos[i].audio;
-  player.play();
+  player.src = audios[0].audio;
 }
 
 loadGuide();
